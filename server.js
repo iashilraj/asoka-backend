d182ee080e473e2448b1751be81a4d3ce4180daf
artshil <iashilraj@gmail.com>
2026-02-11 22:04:10 +0530

    Update port config

--- server.js
+++ server.js
@@ -53,5 +53,5 @@ app.delete("/patients/:id", async (req, res) => {
 });
 
 /* ---------- Server ---------- */
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
